c105ab6f37db2b08dc3e8b9bed50bccfd7a15e50
palpurbita <purbita.july17.1997@gmail.com>
2020-09-29 13:05:15 +0530

    second commit

---
+++ all_work/Project2/src/Employee.java
@@ -0,0 +1,67 @@
+
+public class Employee {
+
+	int id;
+	int b_salary;
+	String name;
+	
+	static int ctr=100;
+	
+	public Employee(){
+		ctr++;
+		id=789;
+		b_salary=50000;
+		name="Purbita";
+		System.out.println("Default constructor");
+				
+		
+		
+	}
+	
+	public Employee (int aid,int sal,String Name){
+		ctr++;
+		id=aid;
+		b_salary=sal;
+		name=Name;
+		System.out.println("Parameterized constructor");
+	}
+	
+public void showCtr() {
+		
+		System.out.println("No. of objects of employee type: "+ctr);
+	}
+	
+	public void setData(int i,String n,int b) {
+		
+		id=i;
+		name=n;
+		b_salary=b;
+	}
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	
+	public String getNn() {
+		return name;
+	}
+	
+	public int getSal() {
+		return b_salary;
+	}
+	
+	/*public void displayData() {
+		
+		System.out.println("Id & Salary of "+name+ " is "+id+" & "+b_salary);
+	}*/
+	
+	void employeeDetail() {
+		System.out.println(id);
+		System.out.println(b_salary);
+		System.out.println(name);
+	}
+	
+	
+}
---
+++ all_work/Project2/src/User.java
@@ -0,0 +1,27 @@
+
+public class User {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Employee e1=new Employee();
+		e1.showCtr();
+		e1.employeeDetail();
+		Employee e2=new Employee(1234,10000,"Purbita");
+		e2.showCtr();
+		e2.employeeDetail();
+		//Employee e3=new Employee();
+		/*e1.setData(1,"Purbita",10000);
+		e2.setData(2,"Madhushree",15000);
+		e3.setData(3,"Sananda",20000);
+		e1.displayData();
+		e2.displayData();
+		e3.displayData();*/
+		
+		
+		
+		
+		
+		
+	}
+	}
